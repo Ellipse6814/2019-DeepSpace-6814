5f8510540ffd598a2abb6738c41b00cbdbe7a411
Sean02 <33432158+Sean-2002@users.noreply.github.com>
2019-02-03 15:26:30 -0800

    networkTables

--- src/main/java/frc/robot/subsystems/NetworkTables.java
+++ src/main/java/frc/robot/subsystems/NetworkTables.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  * An example subsystem. You can replace me with your own Subsystem.
  */
-public class ExampleSubsystem extends Subsystem {
+public class NetworkTables extends Subsystem { // TODO:
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
