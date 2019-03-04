786adc3822aa51d3e1bf3772bfa5a730b637edf5
Sean02 <33432158+Sean-2002@users.noreply.github.com>
2019-03-03 16:35:12 -0800

    added MSLifeCm

--- src/main/java/frc/robot/path/RobotPathConfig.java
+++ src/main/java/frc/robot/path/RobotPathConfig.java
@@ -40,4 +40,19 @@ public class RobotPathConfig {
         return practiceRobotConfig;
     }
 
+    public static RobotPathConfig getRobotConfig() { // TODO:
+        RobotPathConfig practiceRobotConfig = new RobotPathConfig();
+        practiceRobotConfig.maxVel = Units.ft2m(9); // 7 before
+        practiceRobotConfig.maxAcc = Units.ft2m(4); // m/sec every sec
+        practiceRobotConfig.spacing = Units.ft2m(1);
+        practiceRobotConfig.maxAngVel = 2; // radians per second
+        practiceRobotConfig.lookAheadDistance = Units.ft2m(1.8);
+        practiceRobotConfig.trackWidth = Units.in2m(25);// 23 inches
+        practiceRobotConfig.targetTolerance = Units.in2m(2);// m
+        practiceRobotConfig.physicalMaxVel = Units.ft2m(12);
+        practiceRobotConfig.kV = 1 / practiceRobotConfig.physicalMaxVel;
+        practiceRobotConfig.kA = 2 / 9;
+        practiceRobotConfig.kP = 0.2;
+        return practiceRobotConfig;
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/LifeCam.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+import edu.wpi.cscore.MjpegServer;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoMode;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class LifeCam extends Subsystem {
+
+    private static LifeCam instance;
+
+    public static LifeCam getInstance() {
+        if (instance == null)
+            instance = new LifeCam();
+        return instance;
+    }
+
+    private UsbCamera camera;
+
+    private LifeCam() {
+        startCamera();
+    }
+
+    public void startCamera() {
+        camera = CameraServer.getInstance().startAutomaticCapture();
+        camera.setVideoMode(VideoMode.PixelFormat.kMJPEG, 320, 240, 15);
+        MjpegServer cameraServer = new MjpegServer("Camera 0", 5810);
+        cameraServer.setSource(camera);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+    }
+}
