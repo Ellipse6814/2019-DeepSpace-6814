5efca6f409f5f117d65a41fa62e77104d5ba3591
Sean-2002 <33432158+Sean-2002@users.noreply.github.com>
2019-03-16 10:48:34 -0700

    tele turning changed from 0.2 to 0.17

--- src/main/java/frc/robot/Const.java
+++ src/main/java/frc/robot/Const.java
@@ -157,7 +157,7 @@ public class Const {
     public static final double kDriveJoystickDeadband = 0.05;
 
     public static final double[] kDrivePowerGears = { 0.3333, 0.6666, 1 };
-    public static final double[] kDriveTurnGears = { 0.2, 0.25, 0.3 };
+    public static final double[] kDriveTurnGears = { 0.17, 0.25, 0.3 };
 
     public static final double maxAccelMSS = 100;
 
