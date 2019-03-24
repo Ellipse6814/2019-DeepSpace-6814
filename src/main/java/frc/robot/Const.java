a9bbf36d75c828fbc7f7d4fb7e2dc69b7f6ca1df
Sean-2002 <33432158+Sean-2002@users.noreply.github.com>
2019-03-24 12:45:50 -0700

    Changed auton and back hatch angle

--- src/main/java/frc/robot/Const.java
+++ src/main/java/frc/robot/Const.java
@@ -100,7 +100,7 @@ public class Const {
         else if (state == ArmState.FrontHatchInOut)
             angle = 0;
         else if (state == ArmState.BackHatchInOut)
-            angle = 144;
+            angle = 143;
         else if (state == ArmState.Middle)
             angle = 90;
         else {
@@ -128,7 +128,7 @@ public class Const {
     public static double calcJawAngle(JawState state) {
         double angle;
         if (state == JawState.Back)
-            angle = 19;
+            angle = 18;
         else if (state == JawState.Front)
             angle = 0;
         else if (state == JawState.Ball)
